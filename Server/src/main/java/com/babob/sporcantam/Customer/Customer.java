20fc4f0ddce9b2952b760e6b2ba9060d1a408c76
ZE0TRON <cmpbilge@gmail.com>
2019-03-18 17:04:41 +0300

    Customer server model added. Methods implemented.

--- Server/src/main/java/com/babob/sporcantam/Customer/Customer.java
+++ Server/src/main/java/com/babob/sporcantam/Customer/Customer.java
@@ -1,6 +1,40 @@
 package com.babob.sporcantam.Customer;
+import com.babob.sporcantam.User.User;
 
-public class Customer {
 
+public class Customer extends User{
+    private String address;
+    private int cartID;
+    private String orderListID;
 
+    public int getCartID() {
+        return cartID;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getOrderListID() {
+        return orderListID;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setCartID(int cartID) {
+        this.cartID = cartID;
+    }
+
+    public void setOrderListID(String orderListID) {
+        this.orderListID = orderListID;
+    }
+
+    @Override
+    public String toString() {
+        String string = "";
+        string = string + "Cart ID :"+cartID+"\n Address: "+address+"\n Order List ID : "+orderListID+"\n Cart ID: "+cartID+"\n";
+        return super.toString()+string;
+    }
 }
--- Server/src/main/java/com/babob/sporcantam/User/User.java
+++ Server/src/main/java/com/babob/sporcantam/User/User.java
@@ -5,4 +5,43 @@ public class User {
     private String first_name;
     private String last_name;
     private String password;
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        String string = "";
+        string = string+"Email : "+email +"\n First Name: "+first_name+"\n Last Name: "+last_name+"\n"+"password:"+password+"\n";
+        return super.toString()+string;
+    }
 }

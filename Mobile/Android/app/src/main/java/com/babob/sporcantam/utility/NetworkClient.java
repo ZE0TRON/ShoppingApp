fa37954e43704985da9f971eff40c555ffb8edb7
BatuhanHU <batuhanmete67@gmail.com>
2019-06-07 17:45:44 +0300

    added multiple sending stuff

---
+++ Mobile/Android/app/src/main/java/com/babob/sporcantam/utility/NetworkClient.java
@@ -0,0 +1,32 @@
+package com.babob.sporcantam.utility;
+
+import android.content.Context;
+
+import com.babob.sporcantam.R;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class NetworkClient {
+
+    private static final String BASE_URL = "http://134.209.226.138:8080/customer/uploadFile";
+    private static Retrofit retrofit;
+
+    public static Retrofit getRetrofitClient(Context context) {
+
+        if (retrofit == null) {
+            OkHttpClient okHttpClient = new OkHttpClient.Builder()
+                    .build();
+            retrofit = new Retrofit.Builder()
+                    .baseUrl(BASE_URL)
+                    .client(okHttpClient)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
+        }
+
+        return retrofit;
+    }
+
+}

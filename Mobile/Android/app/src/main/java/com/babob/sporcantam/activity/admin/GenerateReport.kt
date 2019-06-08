8f68cb5a20cb89fdfcb3658453dccf6e26525768
Nur Bengisu <bengisu.cam@gmail.com>
2019-06-08 19:08:11 +0300

    credit card

---
+++ Mobile/Android/app/src/main/java/com/babob/sporcantam/activity/admin/GenerateReport.kt
@@ -0,0 +1,12 @@
+package com.babob.sporcantam.activity.admin
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class GenerateReport : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_generate_report)
+    }
+}
